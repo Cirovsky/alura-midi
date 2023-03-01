4e24286c2c5772cfc1a28b4d1aca13673a24e1f1
Cirovsky <cirovisqui@gmail.com>
2023-03-01 20:30:07 -0300

    formatação do código javascript

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 const sons = document.querySelectorAll(".tecla");
 
-function tocarSom (som){
+function tocarSom(som) {
     const verificarsom = document.querySelector(som);
-    if (verificarsom != null){
-        if(verificarsom.localName ==='audio'){
+    if (verificarsom != null) {
+        if (verificarsom.localName === 'audio') {
             return document.querySelector(som).play();
-        }else{
+        } else {
             console.log("o elemento selecionado não reproduz áudio");
         }
-    }else{
+    } else {
         console.log("o elemento selecionado não existe");
     }
 }
 
-for( let t in sons){
+for (let t in sons) {
     const som = sons[t]
     som.onclick = () => this.tocarSom(`#som_${som.classList[1]}`);
     som.onkeydown = (e) => {
-        if(e.code == 'Space' || e.code == 'Enter'){
+        if (e.code == 'Space' || e.code == 'Enter') {
             som.classList.add("ativa");
         }
     }
-    som.onkeyup= ( ) => som.classList.remove("ativa");
+    som.onkeyup = () => som.classList.remove("ativa");
 }
 
-function teclar (tecla){
+function teclar(tecla) {
     const somCorrespondente = document.getElementById(tecla.code);
-    if(somCorrespondente == null){
+    if (somCorrespondente == null) {
         return
-    }else{
+    } else {
         somCorrespondente.onclick();
         somCorrespondente.classList.add("ativa");
     }
 }
 
-function desteclar(tecla){
+function desteclar(tecla) {
     const somCorrespondente = document.getElementById(tecla.code);
-    if(somCorrespondente == null){
+    if (somCorrespondente == null) {
         return
-    }else{
+    } else {
         somCorrespondente.classList.remove("ativa");
     }
 }
 
-document.body.addEventListener("keydown", (e)=> teclar(e))
+document.body.addEventListener("keydown", (e) => teclar(e))
 document.body.addEventListener("keyup", (e) => desteclar(e))
